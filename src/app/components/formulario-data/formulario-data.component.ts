546a12a900bbfee5c6e3242281c952db12b1f287
Jose Daniel Vasquez Garcia <josedaniel.vasquez@globant.com>
2019-01-29 21:42:34 -0500

    Form by data, formGroup, formControl, validators

--- src/app/components/formulario-data/formulario-data.component.ts
+++ src/app/components/formulario-data/formulario-data.component.ts
@@ -1,23 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 @Component({
   selector: 'app-formulario-data',
   templateUrl: './formulario-data.component.html',
-  styles: []
+  styles: [`
+    .ng-invalid.ng-touched:not(form){
+      border: 1px solid red;
+    }
+  `]
 })
 export class FormularioDataComponent implements OnInit {
 
   forma:FormGroup;
 
-  constructor() { }
+  usuario: any = {
+    nombrecompleto: {
+      nombre: 'JoseD',
+      apellido: 'vasquez'
+    },
+    correo: 'josd@gmail.com'
+  }
+
+  constructor() { 
+    console.log(this.usuario)
+   }
 
   ngOnInit() {
     this.forma = new FormGroup({
-      nombre: new FormControl('Jose D'),
-      apellido: new FormControl(),
-      correo: new FormControl() 
+      
+      nombre: new FormControl('', [ Validators.required, Validators.minLength(3) ]),
+      apellido: new FormControl('', Validators.required),
+      correo: new FormControl('', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$') ]) 
     })
   }
 
